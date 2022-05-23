51ba35d133c56e427d03d028716d4184dd51aa14
RinceBoby <101865864+RinceBoby@users.noreply.github.com>
2022-05-23 13:25:41 +0530

    6th Commit

--- lib/interfaces/1 home.dart
+++ lib/interfaces/1 home.dart
@@ -1,9 +1,7 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
-import 'package:chordsic/interfaces/2%20player.dart';
 import 'package:chordsic/interfaces/6%20search.dart';
 import 'package:chordsic/interfaces/mini_player.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:marquee/marquee.dart';
 import 'package:on_audio_query/on_audio_query.dart';
